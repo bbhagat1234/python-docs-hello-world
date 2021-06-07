7cf56aefa79ff0dafd4075f389e1a04a5971bb7c
bbhagat1234 <83924666+bbhagat1234@users.noreply.github.com>
2021-06-08 01:49:33 +0530

    Delete gunicorn.py

--- gunicorn.py
+++
@@ -1,9 +0,0 @@
-bind = '0.0.0.0:8000'
-workers: 2
-worker_class: gevent
-threads: 1
-timeout: 600
-graceful_timeout: 30
-accesslog: gunicorn-access.log
-errorlog: gunicorn-error.log
-loglevel: debug
