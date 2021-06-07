f47fe9df09f079e00103e096fca26ab48fb613b6
bbhagat1234 <83924666+bbhagat1234@users.noreply.github.com>
2021-06-07 14:59:36 +0530

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,15 +2,8 @@ bind = '0.0.0.0:8000'
 workers: 2
 worker_class: gevent
 threads: 1
-worker_connections: 1000
-max_requests: 0
-max_requests_jitter: 0
-timeout: 30
+timeout: 600
 graceful_timeout: 30
-keepalive: 2
-limit_request_line: 4094
-limit_request_fields: 100
-limit_request_field_size: 8190
 accesslog: /var/log/gunicorn-access.log
 errorlog: /var/log/gunicorn-error.log
 loglevel: debug
