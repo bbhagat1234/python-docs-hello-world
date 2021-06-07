317ad638ed85bab34fdaeee9c777c00822b7fea4
bbhagat1234 <83924666+bbhagat1234@users.noreply.github.com>
2021-06-07 15:11:13 +0530

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,7 +4,7 @@ worker_class: gevent
 threads: 1
 timeout: 600
 graceful_timeout: 30
-accesslog: /var/log/gunicorn-access.log
-errorlog: /var/log/gunicorn-error.log
+accesslog: gunicorn-access.log
+errorlog: gunicorn-error.log
 loglevel: debug
 default_proc_name: app:app
