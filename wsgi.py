dd39663159ca5280f1f9e76635a4b7415c704e5b
bbhagat1234 <83924666+bbhagat1234@users.noreply.github.com>
2021-06-08 07:09:00 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from bb import application
-
+gunicorn --bind=0.0.0.0 --worker-class gevent -w 1 app:app --access-logfile /var/log/gunicorn-access.log --error-logfile /var/log/gunicorn-error.log --log-level 'debug'
 if __name__ == "__main__":
     application.run()
